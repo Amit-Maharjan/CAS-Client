cc266a2bddf5cd9ab23c1c38864f8aacfcb6a033
Amit Maharjan <maharjanamit8@gmail.com>
2025-04-28 20:33:55 -0400

    Added port number to be printed in the home page.

--- src/main/java/com/maharjan/cas_client/controllers/HomeController.java
+++ src/main/java/com/maharjan/cas_client/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.maharjan.cas_client.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
@@ -7,8 +9,12 @@ import java.security.Principal;
 @RestController
 public class HomeController {
 
+    @Autowired
+    private ServletWebServerApplicationContext webServerAppCtxt;
+
     @GetMapping("/")
     public String home(Principal principal) {
-        return "Welcome, " + principal.getName();
+        int port = webServerAppCtxt.getWebServer().getPort();
+        return "Welcome, " + principal.getName() + "! (Running on port: " + port + ")";
     }
 }
